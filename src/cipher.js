321094d4c160552da748689bc4f067a93353e498
ivanaolortegui <olorteguimor@gmail.com>
2018-11-29 08:42:44 -0500

    Modificación de estilos y test

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,7 @@
 window.cipher = {
    encode: (offset,string) => {
      let output= '';
+      offset=offset %26;
      for (let i =0; i < string.length;i++){
        if(string.charCodeAt(i)>=65 && string.charCodeAt(i) <=90){
        output= output.concat(String.fromCharCode((string.charCodeAt(i) -65 + parseInt(offset))%26+65))
@@ -19,7 +20,7 @@ window.cipher = {
  } else {
    output=output.concat(string[i]);
  }
-    }  
+    }
    return output;
    },
  };
